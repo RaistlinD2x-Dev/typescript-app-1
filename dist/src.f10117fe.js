0df41e876127916489e7427cc5a3936709c983f6
Jesse Richey <RaistlinD2x.dev@outlook.com>
2021-05-13 16:11:20 -0500

    added faker and built User

--- dist/src.f10117fe.js
+++ dist/src.f10117fe.js
@@ -147,7 +147,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53424" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52023" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
